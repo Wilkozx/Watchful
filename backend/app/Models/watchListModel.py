79317fc6ba600c589b7c6d5e73d72aa055d29ad3
Wilkozx <wilkoisontheceiling@gmail.com>
2024-09-25 15:34:32 +0100

    updated watchListModel

--- backend/app/Models/watchListModel.py
+++ backend/app/Models/watchListModel.py
@@ -15,10 +15,11 @@ class WatchlistModel:
         self.connection.close()
         self.cursor.close()
 
-    def addAnimeToWatchlist(self, mal_id, user_id):
-        sql = "INSERT INTO watchlist (mal_id, user_id) VALUES (%s, %s)"
+    def addAnimeToWatchlist(self, id, user_id, tag_id, englishName, japaneseName, image, episodes, release_date, release_type):
+        sql = "INSERT INTO watchlist (mal_id, user_id, tag_id, english_name, japanese_name, image_url, total_episodes, release_date, release_type) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
         try:
-            self.cursor.execute(sql, (mal_id, user_id,))
+            self.cursor.execute(sql, (id, user_id, tag_id, englishName,
+                                japaneseName, image, episodes, release_date, release_type,))
             self.connection.commit()
             return True
         except Exception as e:
@@ -41,8 +42,21 @@ class WatchlistModel:
         sql = "SELECT * FROM watchlist WHERE user_id = %s"
         self.cursor.execute(sql, (user_id,))
         result = self.cursor.fetchall()
+        json_data = []
+        for row in result:
+            json_data.append({
+                "result_id": row[0],
+                "id": row[1],
+                "tag_id": row[2],
+                "english_name": row[3],
+                "japanese_name": row[4],
+                "image_url": row[5],
+                "total_episodes": row[6],
+                "release_date": row[7],
+                "release_type": row[8]
+            })
         if result:
-            return result
+            return json_data
         else:
             return None
 
